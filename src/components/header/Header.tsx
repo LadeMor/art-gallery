63ea8d850db06e75cf99f6614460cc812f77a755
Sergiy <sergpyatko@gmail.com>
2024-08-28 14:54:02 +0300

    Added animations to left nav menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,12 +7,14 @@ import Container from "../container/Container";
 import iconRight from "../../assets/icons/arrow-right-1.svg";
 import closeIcon from "../../assets/icons/close.svg";
 
+
 const Header = () => {
 
     type ID = number | null;
     type AnimClass = string | null;
 
     const [activeIndex, setActiveIndex] = useState<ID>(null);
+    const [activeMenuItemIndex, setActiveMenuItemIndex] = useState<ID>(null);
     const [isMenuOpen, setIsMenuOpen] = useState<Boolean>(false);
     const [animClass, setAnimClass] = useState<AnimClass>(null);
     const [closeIconAnimClass, setCloseIconAnimClass] = useState<AnimClass>(null);
@@ -26,24 +28,29 @@ const Header = () => {
     }
 
     const links = [
-        { label: "Gallery", to: "/" },
-        { label: "Artists", to: "/artists" },
-        { label: "About me", to: "/about-me" },
+        { label: "Gallery", to: "/", description: "Explore a curated collection of stunning artworks that captivate and inspire, each piece telling its own unique story." },
+        { label: "Artists", to: "/artists", description: "Discover the brilliant minds behind the masterpieces, where creativity knows no bounds and each artist leaves their mark on the world" },
+        { label: "About me", to: "/about-me", description: "Learn more about the passion and vision driving this artistic journey, and the story behind the creation of this inspiring space." },
     ]
 
     const onMenuClick = () => {
-        if(isMenuOpen){
+        if (isMenuOpen) {
             setIsMenuOpen(false);
             setAnimClass("animation-left-menu-pop-in");
-        }else{
+        } else {
             setIsMenuOpen(true);
             setAnimClass("animation-left-menu-pop-up");
         }
     }
 
     const onCloseClick = () => {
-        setIsMenuOpen(false);
-        setAnimClass("animation-left-menu-pop-up");
+        if (isMenuOpen) {
+            setIsMenuOpen(false);
+            setAnimClass("animation-left-menu-pop-in");
+        } else {
+            setIsMenuOpen(true);
+            setAnimClass("animation-left-menu-pop-up");
+        }
     }
 
     const onCloseIconMouseOver = () => {
@@ -53,26 +60,40 @@ const Header = () => {
     const onCloseIconMouseOut = () => {
         setCloseIconAnimClass("close-icon-rotate-over");
     }
+    
+    const onMenuItemMouseOver = (index: ID) => {
+        setActiveMenuItemIndex(index);
+    }
+
+    const onMenuItemMouseOut = () => {
+        setActiveMenuItemIndex(null);
+    }
 
     return (
         <React.Fragment>
-             <div className="dark-panel" style={{display: isMenuOpen ? "block" : "none"}} onClick={onCloseClick}>
-                <div className={`nav-menu ${animClass}`} onClick={onCloseClick}>
-                    <img src={closeIcon} 
-                        className={`close-icon ${closeIconAnimClass}`} 
-                        onClick={onCloseClick} 
-                        onMouseOver={onCloseIconMouseOver} 
-                        onMouseOut={onCloseIconMouseOut}/>
-                    {links.map((item, index) => (
-                        <span className="nav-menu-item">
+
+            <div className={`nav-menu ${animClass}`} onClick={onCloseClick} >
+                <img src={closeIcon}
+                    className={`close-icon ${closeIconAnimClass}`}
+                    onClick={onCloseClick}
+                    onMouseOver={onCloseIconMouseOver}
+                    onMouseOut={onCloseIconMouseOut} />
+                {links.map((item, index) => (
+                    <div className="nav-menu-item" onMouseOver={() => onMenuItemMouseOver(index)}
+                                                    onMouseOut={onMenuItemMouseOut} >
                         <h1>
-                            <Link to={item.to}>
+                            <Link to={item.to} >
                                 {item.label}
                             </Link>
                         </h1>
-                    </span>
-                    ))}
-                </div>
+                        <p className={`nav-menu-item-description ${activeMenuItemIndex === index ?
+                         "animation-nav-menu-item-description-show" : "animation-nav-menu-item-description-hide"}`}
+                        >{item.description}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="dark-panel" style={{ display: isMenuOpen ? "block" : "none" }} onClick={onCloseClick}>
+
             </div>
             <Container>
                 <header>
